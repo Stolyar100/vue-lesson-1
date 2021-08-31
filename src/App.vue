fb593ab0764449f8380ca2938296b89ab1fbf258
Taras Seniv <taras.seniv@inventorsoft.co>
2021-08-31 14:14:25 +0300

    updated files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="title">World time application</h1>
+
+    <zones @update-time="updateTime" @add-time="addTime" />
+
+    <h1 v-for="(time, index) in times" :key="index" class="title">
+      {{ time.hours }}:{{ time.minutes }}:{{ time.seconds }} -
+      {{ time.details.timezone }}
+    </h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Zones from "./components/Zones.vue";
+import timeDetails from "@/mixins/timeDetails.js";
 
 export default {
-  name: 'App',
+  mixins: [timeDetails],
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Zones,
+  },
+  data() {
+    return {
+      times: [],
+      zone: "Europe/Kiev",
+    };
+  },
+  mounted() {
+    this.getTimeDetails().then((timeDetails) => {
+      this.addTime(timeDetails);
+    });
+  },
+  methods: {
+    updateTime(timeDetails) {
+      if (!this.times.length) {
+        this.addTime(timeDetails);
+        return;
+      }
+
+      this.times.splice(this.times.length - 1, 1, timeDetails);
+    },
+    addTime(time) {
+      this.times.push(time);
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.title {
+  text-align: center;
+}
+
+.default-el {
+  border: 1px solid #75c5b7;
+  padding: 10px;
+  cursor: pointer;
+  outline: none;
+  transition: all 0.2s;
+  font-weight: 600;
+  border-radius: 2px;
+  color: #75c5b7;
+  background: #f8fff6;
+}
+
+.btn {
+  background-color: #75c5b7;
+  color: #fff;
+
+  &:active {
+    background-color: #fff;
+  }
+}
+
+.ml-1 {
+  margin-left: 0.2rem;
 }
 </style>
